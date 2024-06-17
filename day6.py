568552845cd400da0a1e9d0524422030d45cf05b
Supriya <sarada2001214@gmail.com>
2024-06-17 18:31:27 +0530

    first commit

---
+++ day6.py
@@ -0,0 +1,11 @@
+number = [1,2,3,4,5,6,7]
+print(number)
+number.append(8)
+print(number)
+number.insert(3,10)
+print(number)
+print(number[1])
+print(number[0:3])
+number.pop(0)
+for items in number:
+    print(items)
