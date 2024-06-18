2c35e564771f79a3b6fb65b1b8bd89cafae2a8f2
Supriya <sarada2001214@gmail.com>
2024-06-18 18:05:45 +0530

    commit

--- day7.py
+++ day7.py
@@ -4,3 +4,12 @@ for items in n:
         print(items)  
 
         print(n) 
+
+
+
+n = [1,2,3,4,5,6,7]
+m = []
+for item in n:
+    m.append(item)
+    print(item)
+    item +=1
---
+++ day8.py
@@ -0,0 +1,6 @@
+n = [1,2,3,4,5,6,7]
+m = []
+for item in n:
+    m.append(item)
+print(m)
+    
